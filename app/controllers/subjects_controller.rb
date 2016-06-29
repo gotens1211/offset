a9955ef6a1f601f43a520b214f5115d94e8023b5
gotens1211 <nikeythe@gmail.com>
2016-06-29 08:46:13 +0530

    total score

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -10,4 +10,16 @@ class SubjectsController < ApplicationController
   def show
     @subject = Subject.find(params[:id])
   end
+
+  def check
+    @score = 0
+
+    ans_array = params[:ans]
+    ans_array.each do |option|
+      qs = Question.find(option.first.to_i)
+      if qs.correct_option.to_i == option.second.to_i
+        @score += 1
+      end
+    end
+  end
 end
