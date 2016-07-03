82d066a5d93450c03ab63a597b104c59a2c073a7
gotens1211 <nikeythe@gmail.com>
2016-07-03 22:43:50 +0530

    new branch

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   post 'subjects/check'
+
+  mount Thredded::Engine => '/forum'
+
 end
